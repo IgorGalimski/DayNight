8dd79c92f4ef198d6efa56403306f239ce8c0cb7
IgorGalimski <igorg@fm-games.com>
2021-12-26 15:25:31 +0200

    Init

---
+++ DayAndNight/DayAndNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayAndNightApp.swift
+//  DayAndNight
+//
+//  Created by Yasser Farahi.
+//
+
+import SwiftUI
+
+@main
+struct DayAndNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
